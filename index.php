c4770fd26012b0d3f6928e1df583d2ba977432c7
Oscar <Davase>
2021-02-01 16:07:14 +0100

    update: header

--- index.php
+++ index.php
@@ -11,33 +11,27 @@
     <link rel="icon" href="assets/image/TREES.png" type="image/x-icon">
     <link rel="stylesheet" href="assets/css/app.css">
 </head>
-
-
 <body>
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
+        <div class="container">
 
             <a class="navbar-brand" href="#">
                 <img src="assets/image/TREES.png" alt="" width="30" height="24" class="d-inline-block align-top">
             </a>
 
-
-            <div class="row">
-            <div class="col-xl-11">
-            <a class="navbar-brand" href="#">
-                <img src="assets/image/TREES.png" alt="" width="30" height="24" class="d-inline-block align-top">
-            </a>
-            </div>
-            <div class="col-xl-1">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
+                <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Accueil</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                           data-bs-toggle="dropdown" aria-expanded="false">
                             Nos arbres
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
@@ -48,8 +42,9 @@
                         </ul>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                           A propos
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                           data-bs-toggle="dropdown" aria-expanded="false">
+                            A propos
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                             <li><a class="dropdown-item" href="#">Qui nous sommes ?</a></li>
@@ -64,16 +59,10 @@
                     </li>
                 </ul>
             </div>
-            </div>
-            </div>
+
         </div>
     </nav>
 </header>
-
-
-
-
-
 
 
 <main>
@@ -83,22 +72,22 @@
 </main>
 
 
-
-
-
-
-
-
-
-
+<footer>
 
 
     <footer class="footer" id="delta">
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+                crossorigin="anonymous"></script>
         <script src="assets/js/app.js"></script>
     </footer>
 
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+            crossorigin="anonymous"></script>
+    <script src="assets/js/app.js"></script>
 
 </body>
 </html>
