628b48d52d31ca3a5a122347b98364a89462c2f4
Oscar <Davase>
2021-02-01 19:09:08 +0100

    update footer

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
                      class="d-inline-block align-top">
             </a>
 
-<span class="d-inline-block align-top">Trees</span>
+            <span class="d-inline-block align-top">Trees</span>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar"
                     aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
@@ -80,10 +80,10 @@
     <div class="container hero_logo">
         <div class="brand_logo">
             <div class="avatar">
-            <img src="assets/image/TREES.png"
-                 class="rounded mx-auto d-block"
-                 alt="logo-site"
-                 title="Trees_logo">
+                <img src="assets/image/TREES.png"
+                     class="rounded mx-auto d-block"
+                     alt="logo-site"
+                     title="Trees_logo">
             </div>
         </div>
         <p class="text-center">Lorem ipsum dolor sit amet, <br>consectetur adipisicing elit. Animi, molestiae!</p>
@@ -95,7 +95,7 @@
 
 <footer class="footer" id="delta">
     <!-- Footer -->
-    <footer class="bg-light text-center text-lg-start">
+    <footer class="bg-secondary text-center text-lg-start text-white">
         <!-- Grid container -->
         <div class="container p-4">
             <!--Grid row-->
@@ -107,8 +107,20 @@
                     <p>
                         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
                         molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
-                        voluptatem veniam, est atque cumque eum delectus sint!
+                        voluptatem veniam, est atque cumque eum delectus sint! Lorem ipsum dolor sit amet.
                     </p>
+                    <div class="social-network">
+                        <a href="#">
+                            <img src="assets/svg/Icon%20awesome-instagram.svg"
+                                 alt="icon_instagram"
+                                 width="35px">
+                        </a>
+                        <a href="#">
+                            <img src="assets/svg/Icon%20awesome-twitter.svg"
+                                 alt="icon_twitter"
+                                 width="35px">
+                        </a>
+                    </div>
                 </div>
                 <!--Grid column-->
 
@@ -118,105 +130,55 @@
 
                     <ul class="list-unstyled mb-0">
                         <li>
-                            <a href="#!" class="text-dark">Link 1</a>
+                            <a href="#!" class="text-white">Accueil</a>
                         </li>
                         <li>
-                            <a href="#!" class="text-dark">Link 2</a>
+                            <a href="#!" class="text-white">Nos arbres</a>
                         </li>
                         <li>
-                            <a href="#!" class="text-dark">Link 3</a>
+                            <a href="#!" class="text-white">A propos</a>
                         </li>
                         <li>
-                            <a href="#!" class="text-dark">Link 4</a>
+                            <a href="#" class="text-white">Acheter</a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-white">Contact</a>
                         </li>
                     </ul>
                 </div>
                 <!--Grid column-->
-<!-- Footer -->
-<footer class="bg-secondary text-center text-lg-start text-white">
-    <!-- Grid container -->
-    <div class="container p-4">
-        <!--Grid row-->
-        <div class="row">
-            <!--Grid column-->
-            <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-                <h5 class="text-uppercase">TREES</h5>
 
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
-                    molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
-                    voluptatem veniam, est atque cumque eum delectus sint! Lorem ipsum dolor sit amet.
-                </p>
-                <div class="social-network">
-                    <a href="#">
-                        <img src="assets/svg/Icon%20awesome-instagram.svg"
-                             alt="icon_instagram"
-                             width="35px">
-                    </a>
-                    <a href="#">
-                        <img src="assets/svg/Icon%20awesome-twitter.svg"
-                             alt="icon_twitter"
-                             width="35px">
-                    </a>
+                <!--Grid column-->
+                <div class="col-lg-3 col-md-6 mb-4 mb-md-0 ">
+                    <h5 class="text-uppercase mb-0">Informations</h5>
+
+                    <ul class="list-unstyled">
+                        <li>
+                            <a href="#!"  class="text-white">Mentions légales</a>
+                        </li>
+                    </ul>
+                    <img width="100px" height="auto" src="assets/image/TREES.png" alt="">
                 </div>
+                <!--Grid column-->
             </div>
-            <!--Grid column-->
-
-            <!--Grid column-->
-            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                <h5 class="text-uppercase">Plan du site</h5>
-
-                <ul class="list-unstyled mb-0">
-                    <li>
-                        <a href="#!" class="text-white">Accueil</a>
-                    </li>
-                    <li>
-                        <a href="#!" class="text-white">Nos arbres</a>
-                    </li>
-                    <li>
-                        <a href="#!" class="text-white">A propos</a>
-                    </li>
-                    <li>
-                        <a href="#" class="text-white">Acheter</a>
-                    </li>
-                    <li>
-                        <a href="#" class="text-white">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <!--Grid column-->
-
-            <!--Grid column-->
-            <div class="col-lg-3 col-md-6 mb-4 mb-md-0 ">
-                <h5 class="text-uppercase mb-0">Informations</h5>
-
-                <ul class="list-unstyled">
-                    <li>
-                        <a href="#!"  class="text-white">Mentions légales</a>
-                    </li>
-                </ul>
-                <img width="100px" height="auto" src="assets/image/TREES.png" alt="">
-            </div>
-            <!--Grid column-->
+            <!--Grid row-->
         </div>
-        <!--Grid row-->
-    </div>
-    <!-- Grid container -->
+        <!-- Grid container -->
 
-    <!-- Copyright -->
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
-        © Copyright 2021 | Site créé par
-        <a class="text-dark" href="index.php">TREES</a>
-    </div>
-    <!-- Copyright -->
-</footer>
-<!-- Footer -->
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+            © Copyright 2021 | Site créé par
+            <a class="text-dark" href="index.php">TREES</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-        crossorigin="anonymous"></script>
-<script src="assets/js/app.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+            crossorigin="anonymous"></script>
+    <script src="assets/js/app.js"></script>
 
 </body>
 </html>
