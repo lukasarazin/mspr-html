d722cd62ca75f3668bb482201412e83c80ca9c56
Luka Sarazin <sarazinluka@gmail.com>
2021-02-01 14:27:37 +0100

    update: index

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <title>Trees</title>
+    <meta name="description" content="La super description">
+    <meta name="author" content="Luka Sarazin - Oscar Davase">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <link rel="stylesheet" href="assets/css/app.css">
 </head>
+
+
 <body>
+
+
 
 <h1>COUCOU</h1>
 
