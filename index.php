dcf6b0232670b4817285d499ffcad6fe385b90fe
Luka Sarazin <sarazinluka@gmail.com>
2021-02-01 14:34:19 +0100

    update: logo

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
     <meta name="author" content="Luka Sarazin - Oscar Davase">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="shortcut icon" href="image/TREES.png" type="image/x-icon">
+    <link rel="icon" href="image/TREES.png" type="image/x-icon">
     <link rel="stylesheet" href="assets/css/app.css">
 </head>
 
