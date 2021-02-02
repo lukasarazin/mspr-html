1a2505f6d15950bfa64337e19da087585cb0b92a
Oscar <Davase>
2021-02-02 10:38:56 +0100

    refactor: cleaning

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 
 <main id="main">
 
-        <button class="button">Accueil</button>
-
-
     <section id="home-hero">
         <div class="container">
 
@@ -50,105 +47,6 @@
     </section>
 </main>
 
-
-<footer class="footer" id="delta">
-    <img src="assets/svg/divisor.svg" alt="" class="mb-0" width="1519">
-    <div class="bg-secondary text-center text-lg-start text-white">
-        <div class="container p-4">
-            <div class="row">
-                <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-                    <h5 class="text-uppercase">TREES</h5>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
-                        molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
-                        voluptatem veniam, est atque cumque eum delectus sint! Lorem ipsum dolor sit amet.
-                    </p>
-                    <div class="social-network">
-
-                        <a href="#">
-                            <img src="assets/svg/Icon%20awesome-instagram.svg"
-                                 alt="icon_instagram"
-                                 width="35px">
-                        </a>
-                        <a href="#">
-                            <img src="assets/svg/Icon%20awesome-twitter.svg"
-                                 alt="icon_twitter"
-                                 width="35px">
-                        </a>
-
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                    <h5 class="text-uppercase">Plan du site</h5>
-                    <ul class="list-unstyled mb-0">
-                        <li>
-                            <a href="#!" class="text-white">Accueil</a>
-                        </li>
-                        <li>
-                            <a href="#!" class="text-white">Nos arbres</a>
-                        </li>
-                        <li>
-                            <a href="#!" class="text-white">A propos</a>
-                        </li>
-                        <li>
-                            <a href="#" class="text-white">Acheter</a>
-                        </li>
-                        <li>
-                            <a href="#" class="text-white">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-lg-3 col-md-6 mb-4 mb-md-0 ">
-                    <h5 class="text-uppercase mb-0">Informations</h5>
-
-                    <ul class="list-unstyled">
-                        <li>
-                            <a href="#!" class="text-white">Mentions légales</a>
-                        </li>
-                    </ul>
-                    <img width="100px" height="auto" src="assets/image/TREES.png" alt="">
-                </div>
-            </div>
-        </div>
-        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
-            © Copyright 2021 | Site créé par
-            <a class="text-white" href="index.php">TREES</a>
-        </div>
-    </div>
-</footer>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-        crossorigin="anonymous"></script>
-<script src="assets/js/app.js"></script>
-</body>
-</html>
-<<<<<<< HEAD
-
-    <div class="symbol">
-        <div class="deathly"></div>
-        <div class="hallows"></div>
-    </div>
-    <div class="container hero_logo">
-        <div class="brand_logo">
-            <div class="avatar">
-                <img src="assets/image/TREES.png"
-                     class="rounded mx-auto d-block"
-                     alt="logo-site"
-                     title="Trees_logo">
-            </div>
-        </div>
-        <p class="text-center">Laissez votre emprunte dans ce monde,<br>choisissez et voyagez avec <strong>votre</strong> arbre.</p>
-        <div class="hero_banner">
-        <img src="assets/image/grey.jpg" alt="" width="200px" height="200px">
-        <img src="assets/image/grey.jpg" alt="" width="200px" height="200px">
-        <img src="assets/image/grey.jpg" alt="" width="200px" height="200px">
-        </div>
-    </div>
-</main>
-
 <?php require_once 'template-parts/footer.php' ?>
 
-=======
-a
->>>>>>> main
+
