0a1007433e41498d3c6c79909789d247c522585c
Oscar <Davase>
2021-02-02 20:30:24 +0100

    update: contact

--- contact.php
+++ contact.php
@@ -4,31 +4,30 @@
         <div class="container">
 
             <div class="brand-logo">
-                <img src="assets/image/TREES.png"
-                     alt="Logo site"
-                     class="rounded mx-auto d-block">
+                <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+                <lottie-player src="https://assets9.lottiefiles.com/private_files/lf30_czbttpus.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"    autoplay></lottie-player>
             </div>
 
             <form>
 
-                <div class="row mb-4">
+                <div class="row mb-5">
                     <div class="col first_name">
-                        <div class="form-outline">
+                        <div class="form">
                             <input type="text" id="form3Example1" class="form-control" placeholder="Prénom"/>
                         </div>
                     </div>
                     <div class="col name">
-                        <div class="form-outline">
+                        <div class="form">
                             <input type="text" id="form3Example2" class="form-control" placeholder="Nom"/>
                         </div>
                     </div>
                 </div>
 
-                <div class="form-outline mb-4">
+                <div class="form mb-5">
                     <input type="text" id="form3Example3" class="form-control" placeholder="Objet" />
                 </div>
 
-                <div class="form-outline mb-4">
+                <div class="form mb-5">
                     <input type="email" id="form3Example3" class="form-control" placeholder="Adresse email" />
                 </div>
 
@@ -36,7 +35,9 @@
                     <textarea name="message" id="message" cols="30" rows="10" placeholder="Votre message"></textarea>
                 </div>
 
-                <button type="submit" class="btn btn-primary btn-block mb-4">Envoyer</button>
+                <div class="button-wrapper">
+                    <button type="submit" class="btn btn-primary btn-block mb-4">Envoyer</button>
+                </div>
 
             </form>
         </div>
