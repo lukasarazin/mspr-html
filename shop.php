291994136a2e35212a7d4f298e35950b85d64983
Oscar <Davase>
2021-02-03 16:54:44 +0100

    update: shop

--- shop.php
+++ shop.php
@@ -15,25 +15,26 @@ $arbres = [
 
 
 
-    <section id="shop">
-        <div class="container">
-            <div class="bill_title">
-                <h1>Mon panier</h1>
-                <span>Ticket N°<?php echo date('YmdHis') . rand(1000, 9999); ?></span>
-            </div>
-
-
-            <div class="bill_content">
-                <?php foreach ($arbres as $key => $arbre): ?>
-                    <div class="test">
-                        <?php foreach ($arbre as $item_key => $item): ?>
-                            <span class="name"><?php echo $item_key; ?></span>
-                            <span class="country_arbre"><?php echo $item['country']; ?></span>
-                            <img src="assets/image/<?php echo $item['picture']; ?>" alt="">
-                        <?php endforeach; ?>
-                    </div>
-                <?php endforeach; ?>
-            </div>
+<section id="shop">
+    <div class="container">
+        <div class="bill_title">
+            <h1>Mon panier</h1>
+            <span>Ticket N°<?php echo date('YmdHis') . rand(1000, 9999); ?></span>
         </div>
-    </section>
+
+
+        <div class="bill_content">
+            <?php foreach ($arbres as $key => $arbre): ?>
+                <div class="test">
+                    <?php foreach ($arbre as $item_key => $item): ?>
+                        <div class="row">
+                            <div class="col-md-3"<img src="assets/image/<?php echo $item['picture']; ?>" alt=""></div>
+                        <div class="col-md-9"<span class="name"><?php echo $item_key. ' ' . $item['country']; ?></span>
+
+                    <?php endforeach; ?>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</section>
 <?php require_once 'template-parts/footer.php' ?>
