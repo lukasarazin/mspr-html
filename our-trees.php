b15927b96a525d145fb66e2bfc29eab24339b17e
Oscar <Davase>
2021-02-03 19:51:06 +0100

    update: our_trees

--- our-trees.php
+++ our-trees.php
@@ -1,46 +1,47 @@
-<?php require_once 'template-parts/header.php' ?>
+<?php require_once 'template-parts/header.php'?>
+<?php
+$products = [
+    'arbres' => [
+        'Baobab' => [
+            'price' => 5,
+            'country'=>'Kenya'],
+
+        'Gingko' => [
+            'price' => 5,
+            'country'=>'Japon'],
+
+        'araukariia' => [
+            'price' => 5,
+            'country'=>'Argentine'],
+
+        'cocotier' => [
+            'price' => 5,
+            'country'=>'Thaiti'],
+    ]
+];
+?>
 
 
-<main>
-<div class="container">
-    <div class="trees effect">
 
-        <div class="tree-img" style='background-image: url("https://storage.journaldemontreal.com/v1/dynamic_resize/sws_path/jdx-prod-images/39f11c85-f901-4b8c-913c-57052f831738_ORIGINAL.jpg?quality=80&size=1200x&version=0");'></div>
 
-        <div class="tree-name">
-            <h2 class="tree-title">baobab</h2>
-        </div>
-
+<section id="our_trees">
+    <div class="container">
+        <?php foreach ($products as $key => $product): ?>
+            <?php foreach ($product as $item_key => $item): ?>
+                <div class="content">
+                    <div class="trees effect">
+                        <div class="tree-img" style='background-image: url("https://storage.journaldemontreal.com/v1/dynamic_resize/sws_path/jdx-prod-images/39f11c85-f901-4b8c-913c-57052f831738_ORIGINAL.jpg?quality=80&size=1200x&version=0");'>
+                        </div>
+                        <div class="tree-name">
+                            <h2 class="tree-title"><?php echo $item_key; ?></h2>
+                            <span class="country"><?php echo $item ['country']; ?></span>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        <?php endforeach; ?>
     </div>
-    <div class="trees effect">
-
-        <div class="tree-img" style='background-image: url("https://img5.goodfon.com/wallpaper/nbig/0/a1/araukariia-brazilskaia-derevia-les-oblaka-tuman.jpg");'></div>
-
-        <div class="tree-name">
-            <h2 class="tree-title">araucaria</h2>
-        </div>
-
-    </div>
-    <div class="trees effect">
-
-        <div class="tree-img" style='background-image: url("https://img.peapix.com/1e03556ff5b54317bd40d6f005d1aca2_480.jpg");'></div>
-
-        <div class="tree-name">
-            <h2 class="tree-title">ginkgo</h2>
-        </div>
-
-    </div>
-    <div class="trees effect">
-
-        <div class="tree-img" style='background-image: url("https://eskipaper.com/images/hawaii-beach-night-2.jpg");'></div>
-
-        <div class="tree-name">
-            <h2 class="tree-title">cocotier</h2>
-        </div>
-
-    </div>
-    </div>
-</main>
+</section>
 
 
 
