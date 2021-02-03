14459b9531c73c39f558a72d23e4c600f87bdb40
Oscar <Davase>
2021-02-03 16:49:15 +0100

    update: shop php

--- shop.php
+++ shop.php
@@ -1,17 +1,39 @@
 <?php require_once 'template-parts/header.php' ?>
-    <div class="container">
-        <div class="bill_title">
-            <h1>Mon panier</h1>
-            <span>Ticket N°<?php echo date('YmdHis') . rand(1000, 9999); ?></span>
-        </div>
-        <div class="bill_content">
-            <div class="row">
-                <div class="col-md-3">
-                    <img src="assets/image/" alt="">
-                    <span></span>
-                </div>
-                <div class="col-md-9">zeze</div>
+<?php
+
+$arbres = [
+    'trees' => [
+        'Baobab' => [
+            'price' => 5,
+            'picture' => 'Baobab.jpg',
+            'country' => 'Kenya'],
+    ]
+];
+?>
+
+
+
+
+
+    <section id="shop">
+        <div class="container">
+            <div class="bill_title">
+                <h1>Mon panier</h1>
+                <span>Ticket N°<?php echo date('YmdHis') . rand(1000, 9999); ?></span>
+            </div>
+
+
+            <div class="bill_content">
+                <?php foreach ($arbres as $key => $arbre): ?>
+                    <div class="test">
+                        <?php foreach ($arbre as $item_key => $item): ?>
+                            <span class="name"><?php echo $item_key; ?></span>
+                            <span class="country_arbre"><?php echo $item['country']; ?></span>
+                            <img src="assets/image/<?php echo $item['picture']; ?>" alt="">
+                        <?php endforeach; ?>
+                    </div>
+                <?php endforeach; ?>
             </div>
         </div>
-    </div>
+    </section>
 <?php require_once 'template-parts/footer.php' ?>
