d5e135c1b2a013fda0ff165cac93c7acf5026796
Luka Sarazin <sarazinluka@gmail.com>
2021-02-05 00:05:17 +0100

    update: 404

---
+++ baobab.php
@@ -0,0 +1,8 @@
+<?php require_once 'template-parts/header.php' ?>
+
+<main id="main-contact">
+
+</main>
+
+<?php require_once 'template-parts/footer.php' ?>
+
---
+++ error-404.php
@@ -0,0 +1,24 @@
+<?php require_once 'template-parts/header.php' ?>
+
+        <section id="404" style="background-color: #fcfcfc;">
+        <div class="container lottie">
+            <div class="row" style="margin-top: 0; margin-bottom: 0;">
+                <div class="col-md-6 align-self-center">
+                    <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+                    <lottie-player src="https://assets3.lottiefiles.com/packages/lf20_vp7o91.json"  background="transparent"  speed="1"  style="max-width: 500px; width: 100%; height: 700px;"  loop  autoplay></lottie-player>
+                </div>
+                <div class="col-md-6 align-self-center">
+                    <h1>404</h1>
+                    <h2 style="color: #3bc36e">VOTRE PAGE A PLANTER.</h2>
+                    <p><strong>La page que vous recherchez n'existe pas.</strong> <br>Vous vous êtes probablement perdu en chemin.
+                        <br><br>
+                        Vous pouvez cliquer sur le bouton ci-dessous pour revenir à la page d'accueil.
+                    </p>
+                    <a href="./index.php"><button class="btn btn-primary">Revenir en lieu sûr</button></a>
+                </div>
+            </div>
+        </div>
+        </section>
+
+<?php require_once 'template-parts/footer.php' ?>
+
