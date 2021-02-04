5b2c249cf662ae45073c27a79234b4f6ecc7ee78
Oscar <Davase>
2021-02-04 12:53:49 +0100

    update: img our_trees

--- data_base.php
+++ data_base.php
@@ -12,18 +12,18 @@ $trees = [
         'name' => 'Gingko',
         'price' => 5,
         'country' => 'Japon',
-        'url' => 'url("https://images5.alphacoders.com/335/thumb-1920-335403.jpg")'
+        'url' => 'url(https://cdn.futura-sciences.com/buildsv6/images/wide1920/f/4/f/f4f0145bcb_50159406_ginkgo-biloba-namezon-adobe-stock.jpg)'
     ],
     'araukaria' => [
         'name' => 'Araukaria',
         'price' => 5,
         'country' => 'Argentine',
-        'url' => 'url("https://images5.alphacoders.com/335/thumb-1920-335403.jpg")'
+        'url' => 'url("https://img5.goodfon.com/wallpaper/nbig/0/a1/araukariia-brazilskaia-derevia-les-oblaka-tuman.jpg");'
     ],
     'cocotier' => [
         'name' => 'Cocotier',
         'price' => 5,
         'country' => 'Thaiti',
-        'url' => 'url("https://images5.alphacoders.com/335/thumb-1920-335403.jpg")'
+        'url' => 'url("https://eskipaper.com/images/hawaii-beach-night-2.jpg")'
     ],
 ];
