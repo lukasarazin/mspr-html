018e08ab94c3766533763440c78e66c518ab579c
Oscar <Davase>
2021-02-05 11:38:57 +0100

    update: bill

--- shop.php
+++ shop.php
@@ -10,28 +10,38 @@ foreach ($cart as $tree):
 endforeach;
 
 require_once 'template-parts/header.php';
+
 ?>
 
-<section id="our_trees">
+<section id="shop">
+    <!--
+        <img class="leave-top-right" src="assets/image/leave-right.png" alt="">
+        <img class="leave-top-left" src="assets/image/leave-left.png" alt="">
+    -->
     <div class="container">
-
-        <h1>Shop</h1>
-
+        <h1>Votre panier</h1>
         <div class="row g-4">
+            <table>
+                <?php foreach ($cartItems as $item): ?>
+                <tbody>
+                <tr>
+                    <td class="trees"><?php echo $item['name']; ?></td>
+                    <td class="trees"><?php echo $item['price']; ?> €</td>
+                </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
 
-            <?php foreach ($cartItems as $item): ?>
-                <div class="col">
-                    <div class="card card-body">
-                        <h3><?php echo $item['name']; ?></h3>
-                    </div>
-                </div>
-            <?php endforeach; ?>
+            <div class="button-wrapper">
+                <a class="btn btn-primary" href="#">
+                    Acheter
+                </a>
+            </div>
 
         </div>
-
-
     </div>
 </section>
+
 
 <?php require_once 'template-parts/footer.php' ?>
 
