83d49bc59bf0cd703e32a45502519d484ac1c881
Luka Sarazin <sarazinluka@gmail.com>
2021-02-12 18:13:58 +0100

    update: conferences

--- baobab.php
+++ baobab.php
@@ -17,11 +17,15 @@
                         <li class="image-item"><img
                                     src="https://mk0darwinnutrit3vtub.kinstacdn.com/wp-content/uploads/2020/03/baobab.jpeg">
                         </li>
-                        <li class="image-item"><img src="https://media-cdn.tripadvisor.com/media/photo-s/18/ab/b1/af/suite.jpgs"></li>
+                        <li class="image-item"><img
+                                    src="https://cdn.futura-sciences.com/buildsv6/images/wide1920/0/1/4/0142dbcd83_126217_baobab-afrique.jpg">
+                        </li>
                         <li class="image-item"><img src="https://source.unsplash.com/5WbYFH0kf_8"></li>
                     </ul>
                 </div>
-                <a href="./planter-mon-arbre.php"><button class="btn btn-primary">Planter mon Baobab</button></a>
+                <a href="./planter-mon-arbre.php">
+                    <button class="btn btn-primary">Planter mon Baobab</button>
+                </a>
             </div>
 
             <div>
@@ -29,34 +33,41 @@
 
                 <section id="second">
                     <div class="description">
-                            <h3>Description</h3>
+                        <h3>Description</h3>
 
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-                                eligendi eveniet harum, ipsum itaque iure labore optio possimus quia quidem,
-                                sit ut voluptates. Animi commodi deserunt dignissimos dolor dolore dolores
-                                eligendi enim error est illo impedit iure labore magni maxime natus nemo omnis
-                                optio quaerat quia sit ullam veniam, vitae.</p>
+                        <p>Le baobab est un arbre qui émerge de la savane, résiste à
+                            des chaleurs intenses, peut vivre très vieux et mesurer
+                            40 mètres de haut.</p>
 
-                            <h3>Origine</h3>
+                        <h3>Origine</h3>
 
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-                                eligendi eveniet harum, ipsum itaque iure labore optio possimus quia quidem,
-                                sit ut voluptates. Animi commodi deserunt dignissimos dolor dolore dolores
-                                eligendi enim error est illo impedit iure labore magni maxime natus nemo omnis
-                                optio quaerat quia sit ullam veniam, vitae.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+                            eligendi eveniet harum, ipsum itaque iure labore optio possimus quia quidem,
+                            sit ut voluptates. Animi commodi deserunt dignissimos dolor dolore dolores
+                            eligendi enim error est illo impedit iure labore magni maxime natus nemo omnis
+                            optio quaerat quia sit ullam veniam, vitae.</p>
 
 
-                            <h3>Particularité</h3>
+                        <h3>Particularité</h3>
 
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-                                eligendi eveniet harum, ipsum itaque iure labore optio possimus quia quidem,
-                                sit ut voluptates. Animi commodi deserunt dignissimos dolor dolore dolores
-                                eligendi enim error est illo impedit iure labore magni maxime natus nemo omnis
-                                optio quaerat quia sit ullam veniam, vitae.</p>
+                        <p>En Afrique, le baobab est appelé "l'arbre magique", "l'arbre pharmacien" ou encore "l'arbre de la vie". Les différentes parties de cet arbre sont en effet utilisées dans l’alimentation et dans la pharmacopée traditionnelle africaine.
+
+                            Les fruits, les graines et les feuilles des baobabs sont comestibles :
+                            • les fruits appelés "pain de singe" ont un goût acidulé et sont très riches en vitamines et en calcium
+                            • les graines se consomment grillées et on peut en extraire une huile alimentaire
+                            • les jeunes pousses et les racines des jeunes plants se mangent comme des asperges
+                            • les feuilles se consomment bouillies ou séchées.
+
+                            Le baobab entre dans la composition de nombreux remèdes :
+                            • la décoction de la pulpe sèche du fruit est utilisée comme antidiarrhéique et comme fébrifuge
+                            • la décoction de feuille est utilisée contre le paludisme
+
+                            L’écorce fibreuse de cet arbre sert à confectionner cordes et cordages. En revanche, son bois est trop mou et gorgé d'eau pour être utilisé.</p>
 
                     </div>
 
-                    <iframe class="container" width="1156" height="538" src="https://www.youtube.com/embed/ebOLqfeFhg0" frameborder="0"
+                    <iframe class="container" width="1156" height="538" src="https://www.youtube.com/embed/ebOLqfeFhg0"
+                            frameborder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                             allowfullscreen></iframe>
 
---
+++ nos-conferences.php
@@ -0,0 +1,31 @@
+<?php require_once 'template-parts/header.php' ?>
+
+
+<section id="conference">
+
+    <div class="container-fluid">
+
+    <div class="row">
+        <div class="col-lg-4"><img src="assets/image/500.gif" height="350" width="494" alt=""></div>
+        <div class="col-lg-4"><h2>Titre 1</h2><h4>Titre 2</h4><p>Startups, collaborateurs·rices porteurs·euses de projets, corporates, étudiants·es, journalistes ou investisseurs·euses</p></div>
+        <div class="col-lg-4"><img src="assets/image/500.gif" height="350" width="494" alt=""></div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-2"><h2>Titre 1</h2><h4>Titre 2</h4><p>À travers 6 univers thématiques : Market, Tech, Entrepreneurship, Society, Work, Design...</p></div>
+        <div class="col-lg-4"><img src="assets/image/500.gif" height="350" width="494" alt=""></div>
+        <div class="col-lg-2"><h2>Titre 1</h2><h4>Titre 2</h4><p>Inspirés·es, experts·es, talentueux·euses, ils·elles viennent délivrer des messages différents à tous nos publics.</p></div>
+        <div class="col-lg-4"><img src="assets/image/500.gif" height="350" width="494" alt=""></div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-3"><img src="assets/image/500.gif" height="350" width="370" alt=""></div>
+        <div class="col-lg-3"><h2>Titre 1</h2><h4>Titre 2</h4><p>Incontestablement, the place to be pour faire parler de vous !</p></div>
+        <div class="col-lg-3"><img src="assets/image/500.gif" height="350" width="370" alt=""></div>
+        <div class="col-lg-3"><h2>Titre 1</h2><h4>Titre 2</h4><p></p></div>
+    </div>
+    </div>
+</section>
+
+
+<?php require_once 'template-parts/footer.php' ?>
