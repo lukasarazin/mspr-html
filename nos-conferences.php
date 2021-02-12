721561890e75b045a7741795731bef0dc752c2c3
Luka Sarazin <sarazinluka@gmail.com>
2021-02-12 18:38:30 +0100

    update: p

--- nos-conferences.php
+++ nos-conferences.php
@@ -3,25 +3,26 @@
 
 <section id="conference">
 
+    <img src="http://welikeit.fr/wp-content/uploads/2015/06/ezgif-1367805649.gif" alt="">
+
     <div class="container-fluid">
-
+        
     <div class="row">
-        <div class="col-lg-4"><img src="assets/image/500.gif" height="350" width="494" alt=""></div>
+        <div class="col-lg-4"><img class="img-conf" src="https://media.parisladefense.com/9WfhgBi85lOJp0MA41bVr-QyIxI=/fit-in/1200x0/dossiers/evenementiel/garden-parvis-2019-j1-c-carlos-ayesta-140.JPG" height="350" width="494" alt=""></div>
         <div class="col-lg-4"><h2>Titre 1</h2><h4>Titre 2</h4><p>Startups, collaborateurs·rices porteurs·euses de projets, corporates, étudiants·es, journalistes ou investisseurs·euses</p></div>
-        <div class="col-lg-4"><img src="assets/image/500.gif" height="350" width="494" alt=""></div>
+        <div class="col-lg-4"><img class="img-conf4" src="assets/image/500.gif" height="700" width="494" alt=""></div>
     </div>
 
     <div class="row">
-        <div class="col-lg-2"><h2>Titre 1</h2><h4>Titre 2</h4><p>À travers 6 univers thématiques : Market, Tech, Entrepreneurship, Society, Work, Design...</p></div>
-        <div class="col-lg-4"><img src="assets/image/500.gif" height="350" width="494" alt=""></div>
-        <div class="col-lg-2"><h2>Titre 1</h2><h4>Titre 2</h4><p>Inspirés·es, experts·es, talentueux·euses, ils·elles viennent délivrer des messages différents à tous nos publics.</p></div>
-        <div class="col-lg-4"><img src="assets/image/500.gif" height="350" width="494" alt=""></div>
+        <div class="col-lg-2"><h2>70</h2><h4>Conférences</h4><p>À travers 6 univers thématiques : Market, Tech, Entrepreneurship, Society, Work, Design...</p></div>
+        <div class="col-lg-4"><img class="img-conf" src="assets/image/500.gif" height="350" width="494" alt=""></div>
+        <div class="col-lg-2"><h2>30</h2><h4>Speakers</h4><p>Inspirés·es, experts·es, talentueux·euses, ils·elles viennent délivrer des messages différents à tous nos publics.</p></div>
     </div>
 
     <div class="row">
-        <div class="col-lg-3"><img src="assets/image/500.gif" height="350" width="370" alt=""></div>
+        <div class="col-lg-3"><img class="img-conf" src="assets/image/500.gif" height="350" width="370" alt=""></div>
         <div class="col-lg-3"><h2>Titre 1</h2><h4>Titre 2</h4><p>Incontestablement, the place to be pour faire parler de vous !</p></div>
-        <div class="col-lg-3"><img src="assets/image/500.gif" height="350" width="370" alt=""></div>
+        <div class="col-lg-3"><img class="img-conf" src="https://static.wixstatic.com/media/a27d24_feb617eec5fb4095ad0284444b6cbb6a~mv2.jpg/v1/fill/w_506,h_409,al_c,q_90/a27d24_feb617eec5fb4095ad0284444b6cbb6a~mv2.jpg" height="350" width="370" alt=""></div>
         <div class="col-lg-3"><h2>Titre 1</h2><h4>Titre 2</h4><p></p></div>
     </div>
     </div>
