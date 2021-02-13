05fa0d791c676dddc0ba84957ba7a07024c318ae
Oscar <Davase>
2021-02-13 20:49:38 +0100

    update : shop

--- shop.php
+++ shop.php
@@ -11,21 +11,25 @@ endforeach;
 
 require_once 'template-parts/header.php';
 
+$total = 0;
+
 ?>
 
 <section id="shop">
-    <!--
-        <img class="leave-top-right" src="assets/image/leave-right.png" alt="">
-        <img class="leave-top-left" src="assets/image/leave-left.png" alt="">
-    -->
+
+    <img class="leave-top-right" src="assets/image/leave-right.png" alt="">
+    <img class="leave-top-left" src="assets/image/leave-left.png" alt="">
+
     <div class="container">
         <h1>Votre panier</h1>
         <div class="row g-4">
             <table>
-                <?php foreach ($cartItems as $item): ?>
+                <?php foreach ($cartItems
+
+                as $item): ?>
                 <tbody>
                 <tr>
-                    <td class="trees"><?php echo $item['name']; ?></td>
+                    <td class="trees">· <?php echo $item['name']; ?></td>
                     <td class="trees"><?php echo $item['price']; ?> €</td>
                 </tr>
                 <?php endforeach; ?>
