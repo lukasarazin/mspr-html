ebb55956dd3e9fe3a5ccdff14df583ce2e98dd35
Oscar <Davase>
2021-02-21 17:01:00 +0100

    update: plant-tree

--- data_base.php
+++ data_base.php
@@ -9,26 +9,26 @@ $trees = [
         'url' => 'https://images5.alphacoders.com/335/thumb-1920-335403.jpg',
         'img'=> 'assets/image/card-1.jpg'
     ],
-    'gingko' => [
-        'name' => 'Gingko',
+    'ginkgo' => [
+        'name' => 'Ginkgo',
         'price' => 5,
         'country' => 'Japon',
         'url' => 'https://cdn.futura-sciences.com/buildsv6/images/wide1920/f/4/f/f4f0145bcb_50159406_ginkgo-biloba-namezon-adobe-stock.jpg',
-        'img'=> 'assets/image/card-1.jpg'
+        'img'=> 'assets/image/card-2.jpg'
     ],
-    'araukaria' => [
-        'name' => 'Araukaria',
+    'araucaria' => [
+        'name' => 'Araucaria',
         'price' => 5,
         'country' => 'Argentine',
         'url' => 'https://img5.goodfon.com/wallpaper/nbig/0/a1/araukariia-brazilskaia-derevia-les-oblaka-tuman.jpg',
-        'img'=> 'assets/image/card-1.jpg'
+        'img'=> 'assets/image/card-3.jpg'
     ],
     'cocotier' => [
         'name' => 'Cocotier',
         'price' => 5,
         'country' => 'Thaiti',
         'url' => 'https://eskipaper.com/images/hawaii-beach-night-2.jpg',
-        'img'=> 'assets/image/card-1.jpg'
+        'img'=> 'assets/image/card-4.jpg'
     ],
 ];
 
--- planter-mon-arbre.php
+++ planter-mon-arbre.php
@@ -12,7 +12,7 @@ require_once 'template-parts/header.php';
 
 <section id="our_trees">
     <div class="container">
-
+        <!--
         <div class="home_cards">
             <div class="row g-4 ">
                 <div class="col-lg-4">
@@ -40,60 +40,29 @@ require_once 'template-parts/header.php';
                 </div>
             </div>
         </div>
+        !-->
 
-
-        <form method="POST">
-            <div class="row g-4">
-                <?php foreach ($trees as $index => $tree): ?>
-
-                    <div class="col-sm-4">
-                        <a href="#" id="tree-<?php echo $index; ?>" class="tree">
-                            <img class="tree-thumbnail" src="<?php echo $tree['img']; ?>" alt="">
-                            <div class="tree-body">
-                                <h2 class="tree-title"><?php echo $tree['name']; ?></h2>
-                                <span class="country"><?php echo $tree['country']; ?></span>
-
-                                <div class="form-group">
-                                    <label class="choose" for="select-tree-<?php echo $index; ?>">Sélectionner</label>
-                                    <input type="checkbox" name="trees[]" id="select-tree-<?php echo $index; ?>"
-                                           value="<?php echo $index; ?>">
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-
-                <?php endforeach; ?>
-            </div>
-
-            <div class="button-wrapper pt-5 text-center">
-                <button class="btn btn-secondary" type="submit">Ajouter au panier</button>
-            </div>
-
-        </form>
-
-    </div>
-</section>
-
-<section id="our-trees">
-    <div class="container">
         <div class="cards-wrapper">
             <form method="POST">
-                <div class="row g-4">
+                <div class="cards_wrapper row">
                     <?php foreach ($trees as $index => $tree): ?>
-                        <div class="content" style="background-image: url('assets/image/card-1.jpg');">
-                            <a href="#" id="tree-<?php echo $index; ?>" class="tree">
-                                <div class="tree-body">
-                                    <h2 class="tree-title"><?php echo $tree['name']; ?></h2>
-                                    <span class="country"><?php echo $tree['country']; ?></span>
+                        <div class="card-tree col-4">
+                            <div class="content">
+                                <a href="#" id="tree-<?php echo $index; ?>" class="tree">
+                                    <img class="tree-thumbnail" src="<?php echo $tree['img']; ?>" alt="">
+                                    <div class="tree-body">
+                                        <h2 class="tree-title"><?php echo $tree['name']; ?></h2>
+                                        <span class="country"><?php echo $tree['country']; ?></span>
 
-                                    <div class="form-group">
-                                        <label class="choose"
-                                               for="select-tree-<?php echo $index; ?>">Sélectionner</label>
-                                        <input type="checkbox" name="trees[]" id="select-tree-<?php echo $index; ?>"
-                                               value="<?php echo $index; ?>">
+                                        <div class="form-group">
+                                            <label class="choose"
+                                                   for="select-tree-<?php echo $index; ?>">Sélectionner</label>
+                                            <input type="checkbox" name="trees[]" id="select-tree-<?php echo $index; ?>"
+                                                   value="<?php echo $index; ?>">
+                                        </div>
                                     </div>
-                                </div>
-                            </a>
+                                </a>
+                            </div>
                         </div>
                     <?php endforeach; ?>
                 </div>
