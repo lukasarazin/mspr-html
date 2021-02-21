25a6de5307c3573e9ea8f4b59cd61cb88d535fba
Oscar <Davase>
2021-02-21 16:22:02 +0100

    update : planter-abres

--- data_base.php
+++ data_base.php
@@ -6,25 +6,29 @@ $trees = [
         'price' => 5,
         'country' => 'Kenya',
         'picture' => 'cover_baboab.php',
-        'url' => 'https://images5.alphacoders.com/335/thumb-1920-335403.jpg'
+        'url' => 'https://images5.alphacoders.com/335/thumb-1920-335403.jpg',
+        'img'=> 'assets/image/card-1.jpg'
     ],
     'gingko' => [
         'name' => 'Gingko',
         'price' => 5,
         'country' => 'Japon',
-        'url' => 'https://cdn.futura-sciences.com/buildsv6/images/wide1920/f/4/f/f4f0145bcb_50159406_ginkgo-biloba-namezon-adobe-stock.jpg'
+        'url' => 'https://cdn.futura-sciences.com/buildsv6/images/wide1920/f/4/f/f4f0145bcb_50159406_ginkgo-biloba-namezon-adobe-stock.jpg',
+        'img'=> 'assets/image/card-1.jpg'
     ],
     'araukaria' => [
         'name' => 'Araukaria',
         'price' => 5,
         'country' => 'Argentine',
-        'url' => 'https://img5.goodfon.com/wallpaper/nbig/0/a1/araukariia-brazilskaia-derevia-les-oblaka-tuman.jpg'
+        'url' => 'https://img5.goodfon.com/wallpaper/nbig/0/a1/araukariia-brazilskaia-derevia-les-oblaka-tuman.jpg',
+        'img'=> 'assets/image/card-1.jpg'
     ],
     'cocotier' => [
         'name' => 'Cocotier',
         'price' => 5,
         'country' => 'Thaiti',
-        'url' => 'https://eskipaper.com/images/hawaii-beach-night-2.jpg'
+        'url' => 'https://eskipaper.com/images/hawaii-beach-night-2.jpg',
+        'img'=> 'assets/image/card-1.jpg'
     ],
 ];
 
