7e78fcda1169a8b1f195bdfa2243af8776ee5f56
Luka Sarazin <sarazinluka@gmail.com>
2021-02-21 21:12:58 +0100

    update: conf

--- nos-conferences.php
+++ nos-conferences.php
@@ -4,7 +4,7 @@
 
 <section id="top-conf">
 
-    <img class="sky" src="http://welikeit.fr/wp-content/uploads/2015/06/ezgif-1367805649.gif" width="1519" height="400" alt="">
+    <img class="sky" src="https://www.laboiteverte.fr/wp-content/uploads/2014/08/cinemagraph-voyage-alberta-canada-04.gif" width="1519" height="400" alt="">
 
 <h1>Nos conférences</h1>
 
@@ -64,21 +64,21 @@
 
         <div class="row">
         <div class="col-lg-6">
-        <p>Actuellement, avec la crise sanitaire, nos conférences sont entièrement maintenues
+        <p>D’ici à fin 2021, il va tout de même se passer pas mal de choses chez Trees, en plein coeur de
+            l’Ile de Nantes. 2021 sera en effet occupée par un déménagement, une programmation annuelle
+            renouvelée de plus 150 événements et une foultitude d’autres projets.
+
+            <br><br>Actuellement, avec la
+            crise sanitaire, nos conférences sont entièrement maintenues
             en ligne. Chaque semaine, notre équipe vous propose des vidéos ou live pour en apprendre davantage
             sur nous et nos ambitions. Nos deux principales conférences se déroulernt
-            comme prévu, devant vous, proche de vous et non dérrière un écran.<br><br>Adipisci aut dolore
-            dolorem enim itaque neque optio, pariatur perspiciatis porro qui quo ratione sit soluta ut vel
-            voluptas voluptatem! Aliquid aperiam asperiores blanditiis consequuntur exercitationem explicabo
-            harum id libero, molestiae nihil nulla quas ratione sunt, ut, voluptatum. </p>
+            comme prévu, devant vous, proche de vous et non dérrière un écran. </p>
         </div>
         <div class="col-lg-6">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores magnam minima
-                mollitia nisi possimus qui. Accusantium aperiam corporis distinctio eligendi
-                facilis necessitatibus, quo! Accusamus aliquid amet animi asperiores aut commodi
-                deserunt dignissimos doloremque doloribus ducimus, eaque eligendi esse et incidunt,
-                ipsa neque nesciunt nostrum nulla officiis perspiciatis qui repellat rerum sequi soluta
-                ullam, vel veniam voluptatibus. <br><br>Adipisci aut dolore dolorem enim itaque neque optio,
+            <p>Depuis la première édition, en juin 2020, nous avons eu à coeur de vous proposer
+                chaque année un événement toujours plus ambitieux. Il nous apparaît cependant que
+                les curseurs sont aujourd’hui poussés à leur maximum (la barre des 11 500
+                participants·es a même été franchie cette année).<br><br>Adipisci aut dolore dolorem enim itaque neque optio,
                 pariatur perspiciatis porro qui quo ratione sit soluta ut vel voluptas voluptatem!
                 Aliquid aperiam asperiores blanditiis consequuntur exercitationem explicabo harum
                 id libero, molestiae nihil nulla quas ratione sunt, ut, voluptatum. </p>
@@ -113,5 +113,82 @@
         </div>
     </section>
 
+    <section id="gifs">
+
+        <h3>Notre actualité</h3>
+
+        <section class="cards container">
+            <article class="card card--1">
+                <div class="card__info-hover">
+                    <svg class="card__like"  viewBox="0 0 24 24">
+                        <path fill="#000000" d="M12.1,18.55L12,18.65L11.89,18.55C7.14,14.24 4,11.39 4,8.5C4,6.5 5.5,5 7.5,5C9.04,5 10.54,6 11.07,7.36H12.93C13.46,6 14.96,5 16.5,5C18.5,5 20,6.5 20,8.5C20,11.39 16.86,14.24 12.1,18.55M16.5,3C14.76,3 13.09,3.81 12,5.08C10.91,3.81 9.24,3 7.5,3C4.42,3 2,5.41 2,8.5C2,12.27 5.4,15.36 10.55,20.03L12,21.35L13.45,20.03C18.6,15.36 22,12.27 22,8.5C22,5.41 19.58,3 16.5,3Z" />
+                    </svg>
+                    <div class="card__clock-info">
+                        <svg class="card__clock"  viewBox="0 0 24 24"><path d="M12,20A7,7 0 0,1 5,13A7,7 0 0,1 12,6A7,7 0 0,1 19,13A7,7 0 0,1 12,20M19.03,7.39L20.45,5.97C20,5.46 19.55,5 19.04,4.56L17.62,6C16.07,4.74 14.12,4 12,4A9,9 0 0,0 3,13A9,9 0 0,0 12,22C17,22 21,17.97 21,13C21,10.88 20.26,8.93 19.03,7.39M11,14H13V8H11M15,1H9V3H15V1Z" />
+                        </svg><span class="card__time">10 min</span>
+                    </div>
+
+                </div>
+                <div class="card__img"></div>
+                <a href="#" class="card_link">
+                    <div class="card__img--hover"></div>
+                </a>
+                <div class="card__info">
+                    <span class="card__category">Voyages</span>
+                    <h3 class="card__title">Période de covid-19</h3>
+                    <span class="card__by">de <a href="#" class="card__author" title="author">Celeste Mills</a></span>
+                </div>
+            </article>
+
+
+            <article class="card card--2">
+                <div class="card__info-hover">
+                    <svg class="card__like"  viewBox="0 0 24 24">
+                        <path fill="#000000" d="M12.1,18.55L12,18.65L11.89,18.55C7.14,14.24 4,11.39 4,8.5C4,6.5 5.5,5 7.5,5C9.04,5 10.54,6 11.07,7.36H12.93C13.46,6 14.96,5 16.5,5C18.5,5 20,6.5 20,8.5C20,11.39 16.86,14.24 12.1,18.55M16.5,3C14.76,3 13.09,3.81 12,5.08C10.91,3.81 9.24,3 7.5,3C4.42,3 2,5.41 2,8.5C2,12.27 5.4,15.36 10.55,20.03L12,21.35L13.45,20.03C18.6,15.36 22,12.27 22,8.5C22,5.41 19.58,3 16.5,3Z" />
+                    </svg>
+                    <div class="card__clock-info">
+                        <svg class="card__clock"  viewBox="0 0 24 24"><path d="M12,20A7,7 0 0,1 5,13A7,7 0 0,1 12,6A7,7 0 0,1 19,13A7,7 0 0,1 12,20M19.03,7.39L20.45,5.97C20,5.46 19.55,5 19.04,4.56L17.62,6C16.07,4.74 14.12,4 12,4A9,9 0 0,0 3,13A9,9 0 0,0 12,22C17,22 21,17.97 21,13C21,10.88 20.26,8.93 19.03,7.39M11,14H13V8H11M15,1H9V3H15V1Z" />
+                        </svg><span class="card__time">5 min</span>
+                    </div>
+
+                </div>
+                <div class="card__img"></div>
+                <a href="#" class="card_link">
+                    <div class="card__img--hover"></div>
+                </a>
+                <div class="card__info">
+                    <span class="card__category">Dômes</span>
+                    <h3 class="card__title">Découvrez Tahiti</h3>
+                    <span class="card__by">de <a href="#" class="card__author" title="author">John Doe</a></span>
+                </div>
+            </article>
+
+            <article class="card card--3">
+                <div class="card__info-hover">
+                    <svg class="card__like"  viewBox="0 0 24 24">
+                        <path fill="#000000" d="M12.1,18.55L12,18.65L11.89,18.55C7.14,14.24 4,11.39 4,8.5C4,6.5 5.5,5 7.5,5C9.04,5 10.54,6 11.07,7.36H12.93C13.46,6 14.96,5 16.5,5C18.5,5 20,6.5 20,8.5C20,11.39 16.86,14.24 12.1,18.55M16.5,3C14.76,3 13.09,3.81 12,5.08C10.91,3.81 9.24,3 7.5,3C4.42,3 2,5.41 2,8.5C2,12.27 5.4,15.36 10.55,20.03L12,21.35L13.45,20.03C18.6,15.36 22,12.27 22,8.5C22,5.41 19.58,3 16.5,3Z" />
+                    </svg>
+                    <div class="card__clock-info">
+                        <svg class="card__clock"  viewBox="0 0 24 24"><path d="M12,20A7,7 0 0,1 5,13A7,7 0 0,1 12,6A7,7 0 0,1 19,13A7,7 0 0,1 12,20M19.03,7.39L20.45,5.97C20,5.46 19.55,5 19.04,4.56L17.62,6C16.07,4.74 14.12,4 12,4A9,9 0 0,0 3,13A9,9 0 0,0 12,22C17,22 21,17.97 21,13C21,10.88 20.26,8.93 19.03,7.39M11,14H13V8H11M15,1H9V3H15V1Z" />
+                        </svg><span class="card__time">10 min</span>
+                    </div>
+
+                </div>
+                <div class="card__img"></div>
+                <a href="#" class="card_link">
+                    <div class="card__img--hover"></div>
+                </a>
+                <div class="card__info">
+                    <span class="card__category">Achats</span>
+                    <h3 class="card__title">Nouvelles espèces</h3>
+                    <span class="card__by">de <a href="#" class="card__author" title="author">Celeste Mills</a></span>
+                </div>
+            </article>
+
+
+        </section>
+        
+    </section>
+    
 </main>
 <?php require_once 'template-parts/footer.php' ?>
