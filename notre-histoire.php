6c1c862aad44444e177f8a10407bec46ebf15607
Oscar <Davase>
2021-02-23 17:40:45 +0100

    update : home

--- notre-histoire.php
+++ notre-histoire.php
@@ -10,15 +10,16 @@
             Mais, celles-ci peuvent avoir des répercussions qu’on ignore, ou plutôt qu’on fait semblant d’ignorer.
             Malheureusement, aujourd’hui nous avons besoin de prendre du recul sur ces avancées technologiques car
             la planète est mourante. Notre oxygène commence à manquer, nos proches commencent à partir plus tôt et
-            notre Terre pourrait bientôt devenir inhabitable. Pourquoi ? Car plus de <strong>40 000 espèces d’arbres vont
-            disparaître</strong> si aucun engagement n’est pris dans les dix prochaines années. </p>
+            notre Terre pourrait bientôt devenir inhabitable. Pourquoi ? Car plus de <strong>40 000 espèces d’arbres
+                vont
+                disparaître</strong> si aucun engagement n’est pris dans les dix prochaines années. </p>
 
         <br><br>
         <p class="col-lg-12">Notre jeune entreprise propose une solution innovante pour sauver la
             planète des problèmes environnementaux qu’elle subit. Nous vous donnons la possibilité
             de posséder votre arbre, de le personnaliser et de choisir parmi plusieurs espèces,
             toutes en voie de disparition. <strong>Vous n'achetez pas un arbre chez Trees, vous lui donnez
-            la vie.</strong> Ce seul geste dans le but de voir un jour ce paysage dont on rêve parfois la nuit.</p>
+                la vie.</strong> Ce seul geste dans le but de voir un jour ce paysage dont on rêve parfois la nuit.</p>
 
         <br><br>
 
@@ -26,23 +27,39 @@
             Aux quatre coins de la planète, nos équipes sont prêtes et travaillent
             chaque jour pour planter des arbres majestueux. Dans plusieurs décennies
             ces arbres domineront notre regard. Notre objectif est donc de <strong>planter un
-            maximum d’arbres</strong>, afin de voir vos enfants ou vos petits-enfants être éblouis
+                maximum d’arbres</strong>, afin de voir vos enfants ou vos petits-enfants être éblouis
             lorsqu’ils verront des terres, urbanisées ou non, remplies d’arbres et de végétation.
         </p>
 
         <details>
             <summary>En savoir plus</summary>
-            L’objectif est de pouvoir faire participer tout le monde pour rendre ce projet efficace le plus rapidement possible.
+            L’objectif est de pouvoir faire participer tout le monde pour rendre ce projet efficace le plus rapidement
+            possible.
             <br><br>
-            Les grands scientifiques s’inquiètent déjà pour l'avenir de l’Homme car nous polluons la planète de plus en plus chaque année. Mais imaginons dans 20 ou 30 ans, à ce moment-là, c’est chaque individu qui s’inquiètera pour sa propre vie et tout le monde se dira “si seulement on avait régi avant”. C’est là que Trees intervient. Notre entreprise prévoit une solution sur le long terme et qui sera de plus en plus efficace chaque année, pour contrer l’augmentation de pollution. Un arbre. Là voilà la solution. Les arbres nous font vivre, mais nous les tuons.
-<br><br>
-            Trees donne la possibilité de planter son arbre, d’acheter son arbre et de la personnaliser. Dans deux ou trois décennies votre arbre sera grand et dominera nos regards. Vous, vos enfants ou encore vos petits enfants auront l’occasion de le voir et de découvrir une partie de la planète comme vous ne l’avez jamais vu.
-<br><br>
-            Afin de planter votre arbre, il suffit de l’acheter, en choisissant d’abord une espèce parmi quatre. Chacune des espèces que nous présentons est en voie de disparition. Pourtant, ces arbres font partie des plus importants pour notre survie. Le baobab, le Ginkgo, l’Araucaria ou encore le cocotier sont connus, mais ne sont pas nombreux.
-<br><br>
-            Le principe est finalement de planter un maximum d’arbres le plus vite possible. Les arbres plantés sont traités avec le soin dont ils ont besoin, dans le climat qui leur convient le mieux et nos plantations respectent à 100% l’écologie. A travers nos quatre biomes donc, nous plantons ces espèces d’arbres et redonnons vie à la nature dont nous avons besoin pour vivre et aujourd’hui survivre.
-<br><br>
-            A côté de ceux-ci, nos maisons en bois naturel vous attendent pour passer un séjour aux côtés de votre arbre.
+            Les grands scientifiques s’inquiètent déjà pour l'avenir de l’Homme car nous polluons la planète de plus en
+            plus chaque année. Mais imaginons dans 20 ou 30 ans, à ce moment-là, c’est chaque individu qui s’inquiètera
+            pour sa propre vie et tout le monde se dira “si seulement on avait régi avant”. C’est là que Trees
+            intervient. Notre entreprise prévoit une solution sur le long terme et qui sera de plus en plus efficace
+            chaque année, pour contrer l’augmentation de pollution. Un arbre. Là voilà la solution. Les arbres nous font
+            vivre, mais nous les tuons.
+            <br><br>
+            Trees donne la possibilité de planter son arbre, d’acheter son arbre et de la personnaliser. Dans deux ou
+            trois décennies votre arbre sera grand et dominera nos regards. Vous, vos enfants ou encore vos petits
+            enfants auront l’occasion de le voir et de découvrir une partie de la planète comme vous ne l’avez jamais
+            vu.
+            <br><br>
+            Afin de planter votre arbre, il suffit de l’acheter, en choisissant d’abord une espèce parmi quatre. Chacune
+            des espèces que nous présentons est en voie de disparition. Pourtant, ces arbres font partie des plus
+            importants pour notre survie. Le baobab, le Ginkgo, l’Araucaria ou encore le cocotier sont connus, mais ne
+            sont pas nombreux.
+            <br><br>
+            Le principe est finalement de planter un maximum d’arbres le plus vite possible. Les arbres plantés sont
+            traités avec le soin dont ils ont besoin, dans le climat qui leur convient le mieux et nos plantations
+            respectent à 100% l’écologie. A travers nos quatre biomes donc, nous plantons ces espèces d’arbres et
+            redonnons vie à la nature dont nous avons besoin pour vivre et aujourd’hui survivre.
+            <br><br>
+            A côté de ceux-ci, nos maisons en bois naturel vous attendent pour passer un séjour aux côtés de votre
+            arbre.
 
         </details>
 
