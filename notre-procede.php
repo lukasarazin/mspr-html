16a19eaf3390f86517247114b0fdcc6564cae963
Oscar <Davase>
2021-02-23 18:52:03 +0100

    update : process

--- notre-procede.php
+++ notre-procede.php
@@ -3,28 +3,30 @@
 
     <section id="process">
         <div class="container">
+            <img class="leave-top-right" src="assets/image/leave-right.png" alt="">
+
+            <img class="leave-top-left" src="assets/image/leave-left.png" alt="">
             <div class="content">
 
                 <div class="step-1 row">
                     <div class="img-step col-lg-6">
-                        <img src="assets/svg/icon%201.svg" alt="">
+                        <img class="lol" src="assets/svg/icon%201.svg" alt="">
                     </div>
                     <div class="text-step col-lg-6">
-                        <h2>Etape 1 :</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem cupiditate eveniet illo ipsam
-                            iusto, laborum, magnam maxime nam necessitatibus nemo obcaecati, pariatur perspiciatis quas
-                            totam.</p>
+                        <h2>Etape 1 : Plantation</h2>
+                        <p>Une fois votre graine acheté sur notre site, notre équipe de professionnelle se charge
+                            ensuite de la planté. </p>
                     </div>
                 </div>
 
                 <div class="step-2 row">
                     <div class="text-step col-lg-6">
-                        <h2>Etape 1 :</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem cupiditate eveniet illo ipsam
-                            iusto, laborum, magnam maxime nam necessitatibus nemo obcaecati, pariatur perspiciatis quas
-                            totam.</p>
+                        <h2>Etape 2 : Développement</h2>
+                        <p>L'entretien de votre arbre est assuré chaque jours par une équipe de professionnelle. Peut
+                            importe les conditions météo, nous entretiendronsvotre arbre avec passsion. Votre arbre doit
+                            pouvoir se développer dans de bonnes conditions le plus naturellement possible.</p>
                     </div>
-                    <div class="img-step col-lg-6">
+                    <div class="img-step-2 col-lg-6">
                         <img src="assets/svg/icon%202.svg" alt="">
                     </div>
                 </div>
@@ -34,10 +36,9 @@
                         <img src="assets/svg/icon%203.svg" alt="">
                     </div>
                     <div class="text-step col-lg-6">
-                        <h2>Etape 1 :</h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem cupiditate eveniet illo ipsam
-                            iusto, laborum, magnam maxime nam necessitatibus nemo obcaecati, pariatur perspiciatis quas
-                            totam.</p>
+                        <h2>Etape 3 : Votre message</h2>
+                        <p>Une fois, que votre arbre à atteind maturité, nous inscrivons votre message sur l'écorce.
+                            Bien entendu, le message ne sera pas graver mais inscrit comme un vrai tatouage.</p>
                     </div>
                 </div>
 
